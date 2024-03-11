2673b583dd4ab270313ba4ed4653358ff80e7555
Irshad <162898323+Irshads001@users.noreply.github.com>
2024-03-11 09:07:34 +0530

    Update vigenere_cipher.c

--- vigenere_cipher.c
+++ vigenere_cipher.c
@@ -1,1 +1,62 @@
-[vigenere_cipher.c](vigenere_cipher.c)
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void vigenereCipherEncrypt(char message[], char key[]);
+void vigenereCipherDecrypt(char message[], char key[]);
+
+int main() {
+    char message[100];
+    char key[100];
+
+    printf("Enter message to encrypt: ");
+    scanf(" %[^\n]s", message);
+
+    printf("Enter encryption key: ");
+    scanf(" %[^\n]s", key);
+
+    // Encrypt the message
+    vigenereCipherEncrypt(message, key);
+    printf("Encrypted message: %s\n", message);
+
+    // Decrypt the message
+    vigenereCipherDecrypt(message, key);
+    printf("Decrypted message: %s\n", message);
+
+    return 0;
+}
+
+void vigenereCipherEncrypt(char message[], char key[]) {
+    int i, j;
+    int messageLen = strlen(message);
+    int keyLen = strlen(key);
+
+    for (i = 0, j = 0; i < messageLen; ++i, ++j) {
+        if (j == keyLen) {
+            j = 0;  // Reset key index if it reaches end
+        }
+
+        if (isalpha(message[i])) {
+            char base = isupper(message[i]) ? 'A' : 'a';
+            message[i] = base + (message[i] - base + key[j] - 'a') % 26;
+        }
+    }
+}
+
+void vigenereCipherDecrypt(char message[], char key[]) {
+    int i, j;
+    int messageLen = strlen(message);
+    int keyLen = strlen(key);
+
+    for (i = 0, j = 0; i < messageLen; ++i, ++j) {
+        if (j == keyLen) {
+            j = 0;  // Reset key index if it reaches end
+        }
+
+        if (isalpha(message[i])) {
+            char base = isupper(message[i]) ? 'A' : 'a';
+            message[i] = base + (message[i] - base - (key[j] - 'a') + 26) % 26;
+        }
+    }
+}
+
